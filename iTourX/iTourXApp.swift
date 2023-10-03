2d7f7083b0365b920211701801d0a4aed476d8e4
Jacek Kosinski <kashiash@gmail.com>
2023-10-03 17:03:39 +0200

    Initial Commit

---
+++ iTourX/iTourXApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourXApp.swift
+//  iTourX
+//
+//  Created by Jacek Kosinski U on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct iTourXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
