8f7317e9d4a296638ae2fb3b8535d597ac725be3
Jacek Kosinski <kashiash@gmail.com>
2023-10-04 10:12:45 +0200

    Dodawanie, modyfikowanie i usuwanie rekordów

---
+++ iTourX/EditDestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  EditDestinationView.swift
+//  iTourX
+//
+//  Created by Jacek Kosinski U on 04/10/2023.
+//
+
+import SwiftUI
+
+struct EditDestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditDestinationView()
+}
